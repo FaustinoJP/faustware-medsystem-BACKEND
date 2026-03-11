52ddabfe81cf428873f9ccb97cee9a389ebf00d2
FaustinoJP <37151061+FaustinoJP@users.noreply.github.com>
2026-03-11 18:08:29 +0100

    Create users.service.ts

---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+      include: {
+        role: true,
+        department: true,
+      },
+    });
+  }
+
+  findById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: {
+        role: true,
+        department: true,
+      },
+    });
+  }
+}
